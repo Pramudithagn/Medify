b5a46f598c0e819451fb31b3344998b1962120e5
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-09-23 23:09:32 +0530

    backend cors config

---
+++ backend/services/gateway/src/main/java/com/pramu/gateway/config/GlobalCorsConfig.java
@@ -0,0 +1,26 @@
+package com.pramu.gateway.config;
+
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(GlobalCorsProperties globalCorsProperties) {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOriginPattern("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
